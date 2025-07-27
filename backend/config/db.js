e84c2bf5c882ef45ed40b7b98a35927bc972914c
HoneyVanjani <honeyvanjani43@gmail.com>
2025-07-27 13:10:11 +0530

    Finalizing backend changes on env branch

---
+++ backend/config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+require('dotenv').config(); // Load environment variables
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      // Optional: Add other connection options if needed
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error connecting to MongoDB: ${error.message}`);
+    process.exit(1); // Exit process with failure
+  }
+};
+
+module.exports = connectDB;
